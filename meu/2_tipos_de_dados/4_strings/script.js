f2b30c463bab104656c543149fb21e9bc617468d
JenniferSantos97 <santos.jennifer1997@outlook.com>
2020-05-19 15:39:46 -0300

    fim da sessão 2

---
+++ meu/2_tipos_de_dados/4_strings/script.js
@@ -0,0 +1,10 @@
+console.log("joão");
+console.log('vida');
+console.log(`aí é fodaaaaaa \n não quero nem saber`);
+console.log("Infinity");
+console.log("salada " + "de " + "batataaaaa");
+console.log(`a soma de 2 + 2 é ${4}`);
+console.log("o meu nome é 'jennifer'");
+console.log('o nome dele é "Matheus"');
+
+
