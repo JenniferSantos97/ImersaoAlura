77c9315b9aa86adc9ec65f3fac798b4b365a982e
JenniferSantos97 <santos.jennifer1997@outlook.com>
2020-06-23 10:34:22 -0300

    aula 1

---
+++ sketch.js
@@ -0,0 +1,12 @@
+let imagemCenario;
+
+function preload(){
+    imagemCenario = loadImage("imagens/cenario/floresta.png")
+}
+
+function setup(){
+    createCanvas(300px,300px);
+}
+function draw(){
+    background(imagemCenario);
+}
